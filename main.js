d7a0cf2a8d6f44ca2834b128925837304578beb5
spartak <spartaksmail.2@gmail.com>
2026-02-23 17:40:49 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let temperature = 26;
+
+switch (true) {
+    case (temperature < 0):
+        console.log("ყინავს!");
+        break;
+
+    case (temperature >= 0 && temperature <= 10):
+        console.log("ძალიან ცივა");
+        break;
+
+    case (temperature >= 11 && temperature <= 20):
+        console.log("ცივა");
+        break;
+
+    case (temperature >= 21 && temperature <= 30):
+        console.log("თბილა");
+        break;
+
+    case (temperature > 30):
+        console.log("ცხელა!");
+        break;
+
+    default:
+        console.log("არასწორი მნიშვნელობა");
+}
